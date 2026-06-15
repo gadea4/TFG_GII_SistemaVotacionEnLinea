df655437fe8679f0f30c2ed7bf5d375e2a13b264
gdp1001 <gdp1001@alu.ubu.es>
2026-06-15 22:49:22 +0200

    update leer certificado v2.2
    Comprobación e impresion de error

--- leer_certificado.py
+++ leer_certificado.py
@@ -3,13 +3,24 @@
 
 import pkcs11
 from pkcs11 import ObjectClass, Attribute 
+import sys
 
 lib = pkcs11.lib(r'C:\Program Files\OpenSC Project\OpenSC\pkcs11\opensc-pkcs11.dll')
+token = None
 
 for slot in lib.get_slots():
-    token = slot.get_token()
-    pin = input()
-    with token.open(user_pin=pin) as session:
+    try: 
+        token = slot.get_token()
+        break
+    except: 
+        pass 
+if token is None: 
+    print("No hay DNI insertado en el lector")
+    sys.exit(1)
+
+pin = input()
+
+with token.open(user_pin=pin) as session:
         certs=list(session.get_objects({Attribute.CLASS: ObjectClass.CERTIFICATE}))
         print("Certificado encontrado")
         for cert in certs: 
