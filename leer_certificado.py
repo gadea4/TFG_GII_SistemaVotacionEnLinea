815a7c8ab8baa6f8f508fb35501778021d9ed676
gdp1001 <gdp1001@alu.ubu.es>
2026-06-15 22:15:21 +0200

    Update leer certificado v2
    Lectura certificado dni

--- leer_certificado.py
+++ leer_certificado.py
@@ -2,10 +2,14 @@
 # Script encargado de detectar los datos del dni 
 
 import pkcs11
+from pkcs11 import ObjectClass, Attribute 
 
 lib = pkcs11.lib(r'C:\Program Files\OpenSC Project\OpenSC\pkcs11\opensc-pkcs11.dll')
 
 for slot in lib.get_slots():
     token = slot.get_token()
     with token.open(user_pin="0000") as session:
-        print("Conexión OK")
+        certs=list(session.get_objects({Attribute.CLASS: ObjectClass.CERTIFICATE}))
+        print("Certificado encontrado")
+        for cert in certs: 
+            print(cert[Attribute.VALUE])
