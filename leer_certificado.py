5c4f928c731dbf5b3bef2289d6e26dd1c64430c7
gdp1001 <gdp1001@alu.ubu.es>
2026-06-24 21:11:34 +0200

    Version final leer ceritifcado.py
    Version final de la lectura del certificado

--- leer_certificado.py
+++ leer_certificado.py
@@ -2,40 +2,88 @@
 # Script encargado de detectar los datos del dni 
 
 import pkcs11
-from pkcs11 import ObjectClass, Attribute 
+from pkcs11 import ObjectClass, Attribute
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
+import json
 import sys
 
-#carga la libreria OpenSC que permite conectar con el lector y el dni
 lib = pkcs11.lib(r'C:\Program Files\OpenSC Project\OpenSC\pkcs11\opensc-pkcs11.dll')
+
 token = None
 
-#El bucle recorre todas las "posibilidades" donde puede estar la tarjeta insertada
 for slot in lib.get_slots():
-    try: 
-        #en caso de que haya tarjeta lo guarda en token y en cuanto la encuentra no sigue mirando mas allá
+    try:
         token = slot.get_token()
         break
-    except: 
-        pass # Si esta vacia lo ignora
-if token is None: #si ha revisado todas y no hay ninguna, concluye que no hay dni
-    print("No hay DNI insertado en el lector")
-    sys.exit(1) #sale del programa
+    except:
+        pass
 
-pin = input() #el usuario introduce el pin y lo guarda en la variable
+if token is None:
+    print("ERROR: No hay DNI insertado en el lector")
+    sys.exit(1)
 
-#abre una sesion con el pin que acabamos de introducir, el with asegura cerrar la sesion
-with token.open(user_pin=pin) as session:
-        certs=list(session.get_objects({Attribute.CLASS: ObjectClass.CERTIFICATE}))
-        print("Certificado encontrado")
+pin = input()
+
+try:
+    with token.open(user_pin=pin) as session:
+
+        certs = list(session.get_objects({Attribute.CLASS: ObjectClass.CERTIFICATE}))
+
         for cert in certs:
-            cert_der=cert[Attribute.VALUE] #obtiene los datos del certificado, pero de forma que no se entiende
-            x509_cert=x509.load_der_x509_certificate(cert_der, default_backend()) # lo convierte en algo entendible
-            subject=x509_cert.subject #obtiene el sujeto, donde estan los datos nombre apellidos dni
-            for attr in subject: 
-                if attr.oid._name == "commonName":
-                 partes = attr.value.split(" , ")
-                 nombre=partes[1].strip()
-                 apellidos=partes[0].strip()
-                 print(nombre, apellidos)
+            cert_der = cert[Attribute.VALUE]
+            x509_cert = x509.load_der_x509_certificate(cert_der, default_backend())
+
+            subject = x509_cert.subject
+
+            datos = {}
+
+            for attr in subject:
+                datos[attr.oid._name] = attr.value
+
+            if "serialNumber" in datos:
+
+                dni = datos.get("serialNumber", "")
+                common_name = datos.get("commonName", "")
+
+                apellido1 = ""
+                apellido2 = ""
+                nombre = ""
+
+                try:
+                    base = common_name.split("(")[0].strip()
+                    partes = base.split(",")
+
+                    if len(partes) == 2:
+                        apellidos = partes[0].strip()
+                        nombre = partes[1].strip()
+
+                        apellidos_split = apellidos.split()
+
+                        if len(apellidos_split) >= 2:
+                            apellido1 = apellidos_split[0]
+                            apellido2 = apellidos_split[1]
+                        elif len(apellidos_split) == 1:
+                            apellido1 = apellidos_split[0]
+
+                except:
+                    pass
+
+                print("CERTIFICADO OK:")
+                print(json.dumps({
+                    "dni":       dni,
+                    "nombre":    nombre,
+                    "apellido1": apellido1,
+                    "apellido2": apellido2
+                }, ensure_ascii=False))
+
+                break
+
+except Exception as e:
+    if "CKR_PIN" in str(e) or "pin" in str(e).lower():
+        print("PIN incorrecto")
+    else: 
+        print(f"ERROR: {str(e)}")
+    sys.exit(1)
+
+
